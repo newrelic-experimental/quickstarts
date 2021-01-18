33741d8cb0c6bb0829967f7ed2bc68b78e05c90b
kidk <samuel@sava.be>
2021-01-18 09:02:55 +0000

    Deploying to gh-pages from  @ 00bbbcb626656279196ad7021989ab5f38ee3cf2 🚀

---
+++ precache-manifest.96102af3a6351080e019c33ce55b2030.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5324c3a835b096c2fe1fea51f0180953",
+    "url": "/quickstarts/index.html"
+  },
+  {
+    "revision": "250d194e178d89b621f2",
+    "url": "/quickstarts/static/css/2.e1483528.chunk.css"
+  },
+  {
+    "revision": "50777f38eeacdd454e20",
+    "url": "/quickstarts/static/css/main.f9915215.chunk.css"
+  },
+  {
+    "revision": "250d194e178d89b621f2",
+    "url": "/quickstarts/static/js/2.1c7982d1.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/quickstarts/static/js/2.1c7982d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50777f38eeacdd454e20",
+    "url": "/quickstarts/static/js/main.9cc7f752.chunk.js"
+  },
+  {
+    "revision": "5563ff35204bb794fd11",
+    "url": "/quickstarts/static/js/runtime-main.67346dca.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/quickstarts/precache-manifest.2fa25d14761e52ed6be6ae52afa31431.js"
+  "/quickstarts/precache-manifest.96102af3a6351080e019c33ce55b2030.js"
 );
 
 self.addEventListener('message', (event) => {
